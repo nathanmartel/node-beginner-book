f198cab199a4318711974f7edbaf7cdb81eabef8
Nathan Martel <nathanmartel@gmail.com>
2020-03-14 22:02:17 -0700

    routing part 2

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ function start(route) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
-    
+    route(handle, pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end()
